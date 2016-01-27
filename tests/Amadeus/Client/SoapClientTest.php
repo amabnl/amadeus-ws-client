f35fd2496a53311fe4cda84a5f2b7f4cf9a83fbe
dieter <dieter.devlieghere@benelux.amadeus.com>
2016-01-27 12:47:34 +0100

    travis doesn't like whitespace differences #2

--- tests/Amadeus/Client/SoapClientTest.php
+++ tests/Amadeus/Client/SoapClientTest.php
@@ -51,8 +51,7 @@ class SoapClientTest extends BaseTestCase
   <ns2:Security>
    <ns2:UsernameToken>
     <ns2:Username>WSBENACS</ns2:Username>
-    <ns2:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest">
-MjAwMDY2MzQ0MmY1MzAzYjU5MDc2YTMrerREZMGVmNzk0Mw==</ns2:Password>
+    <ns2:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest">MjAwMDY2MzQ0MmY1MzAzYjU5MDc2YTMrerREZMGVmNzk0Mw==</ns2:Password>
     <ns2:Nonce>OTk1OTg3Y2DQSDQSDQSEyMWM0MmE1NjQwOTc4NDdjYjFmYWU1MQ==</ns2:Nonce>
     <ns2:Created>2016-01-13T14:56:05:238Z</ns2:Created>
    </ns2:UsernameToken>
@@ -74,8 +73,8 @@ MjAwMDY2MzQ0MmY1MzAzYjU5MDc2YTMrerREZMGVmNzk0Mw==</ns2:Password>
      <ns1:reservation>
       <ns1:controlNumber>ABC123</ns1:controlNumber>
      </ns1:reservation>
-     </ns1:reservationOrProfileIdentifier>
-    </ns1:retrievalFacts>
+    </ns1:reservationOrProfileIdentifier>
+   </ns1:retrievalFacts>
   </ns1:PNR_Retrieve>
  </SOAP-ENV:Body>
 </SOAP-ENV:Envelope>';
@@ -90,18 +89,17 @@ MjAwMDY2MzQ0MmY1MzAzYjU5MDc2YTMrerREZMGVmNzk0Mw==</ns2:Password>
   <ns2:Security>
    <ns2:UsernameToken>
     <ns2:Username>WSBENACS</ns2:Username>
-    <ns2:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest">
-MjAwMDY2MzQ0MmY1MzAzYjU5MDc2YTMrerREZMGVmNzk0Mw==</ns2:Password>
-     <ns2:Nonce>OTk1OTg3Y2DQSDQSDQSEyMWM0MmE1NjQwOTc4NDdjYjFmYWU1MQ==</ns2:Nonce>
-     <ns2:Created>2016-01-13T14:56:05:238Z</ns2:Created>
-    </ns2:UsernameToken>
-   </ns2:Security>
-   <ns3:MessageID>62BEF248-2156-15D2-2056-4044AF358A02</ns3:MessageID>
-   <ns3:To>http://dummy.url</ns3:To>
-   <ns3:Action>http://dummy.soapAction</ns3:Action>
-   <ns4:AMA_SecurityHostedUser>
-    <ns4:UserID POS_Type="1" PseudoCityCode="BRUBG21HE" RequestorType="U"/>
-   </ns4:AMA_SecurityHostedUser>
+    <ns2:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest">MjAwMDY2MzQ0MmY1MzAzYjU5MDc2YTMrerREZMGVmNzk0Mw==</ns2:Password>
+    <ns2:Nonce>OTk1OTg3Y2DQSDQSDQSEyMWM0MmE1NjQwOTc4NDdjYjFmYWU1MQ==</ns2:Nonce>
+    <ns2:Created>2016-01-13T14:56:05:238Z</ns2:Created>
+   </ns2:UsernameToken>
+  </ns2:Security>
+  <ns3:MessageID>62BEF248-2156-15D2-2056-4044AF358A02</ns3:MessageID>
+  <ns3:To>http://dummy.url</ns3:To>
+  <ns3:Action>http://dummy.soapAction</ns3:Action>
+  <ns4:AMA_SecurityHostedUser>
+   <ns4:UserID POS_Type="1" PseudoCityCode="BRUBG21HE" RequestorType="U"/>
+  </ns4:AMA_SecurityHostedUser>
  </SOAP-ENV:Header>
  <SOAP-ENV:Body>
   <ns1:PNR_Retrieve>
