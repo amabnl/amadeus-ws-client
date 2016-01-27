453c1a3b4acf74d8eadf5768c9c7b744c26eca59
dieter <dieter.devlieghere@benelux.amadeus.com>
2016-01-27 12:44:24 +0100

    travis doesn't like whitespace differences

--- tests/Amadeus/Client/SoapClientTest.php
+++ tests/Amadeus/Client/SoapClientTest.php
@@ -87,10 +87,10 @@ MjAwMDY2MzQ0MmY1MzAzYjU5MDc2YTMrerREZMGVmNzk0Mw==</ns2:Password>
         $expected = '<?xml version="1.0"?>
 <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="http://xml.amadeus.com/PNRRET_11_3_1A" xmlns:ns2="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:ns3="http://www.w3.org/2005/08/addressing" xmlns:ns4="http://xml.amadeus.com/2010/06/Security_v1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
  <SOAP-ENV:Header>
-   <ns2:Security>
-    <ns2:UsernameToken>
-     <ns2:Username>WSBENACS</ns2:Username>
-     <ns2:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest">
+  <ns2:Security>
+   <ns2:UsernameToken>
+    <ns2:Username>WSBENACS</ns2:Username>
+    <ns2:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest">
 MjAwMDY2MzQ0MmY1MzAzYjU5MDc2YTMrerREZMGVmNzk0Mw==</ns2:Password>
      <ns2:Nonce>OTk1OTg3Y2DQSDQSDQSEyMWM0MmE1NjQwOTc4NDdjYjFmYWU1MQ==</ns2:Nonce>
      <ns2:Created>2016-01-13T14:56:05:238Z</ns2:Created>
@@ -104,18 +104,18 @@ MjAwMDY2MzQ0MmY1MzAzYjU5MDc2YTMrerREZMGVmNzk0Mw==</ns2:Password>
    </ns4:AMA_SecurityHostedUser>
  </SOAP-ENV:Header>
  <SOAP-ENV:Body>
-   <ns1:PNR_Retrieve>
-    <ns1:retrievalFacts>
-     <ns1:retrieve>
-       <ns1:type>2</ns1:type>
-     </ns1:retrieve>
-     <ns1:reservationOrProfileIdentifier>
-       <ns1:reservation>
-        <ns1:controlNumber>ABC123</ns1:controlNumber>
-       </ns1:reservation>
-     </ns1:reservationOrProfileIdentifier>
-    </ns1:retrievalFacts>
-   </ns1:PNR_Retrieve>
+  <ns1:PNR_Retrieve>
+   <ns1:retrievalFacts>
+    <ns1:retrieve>
+     <ns1:type>2</ns1:type>
+    </ns1:retrieve>
+    <ns1:reservationOrProfileIdentifier>
+     <ns1:reservation>
+      <ns1:controlNumber>ABC123</ns1:controlNumber>
+     </ns1:reservation>
+    </ns1:reservationOrProfileIdentifier>
+   </ns1:retrievalFacts>
+  </ns1:PNR_Retrieve>
  </SOAP-ENV:Body>
 </SOAP-ENV:Envelope>';
 
