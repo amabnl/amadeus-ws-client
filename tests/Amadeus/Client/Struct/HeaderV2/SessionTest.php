aea35160d328c41f09cb4c9823727feaff8b9ce3
dieter <dieter.devlieghere@benelux.amadeus.com>
2016-09-12 11:32:01 +0200

    Unittest for Soap Header 2 Session Header structure.

---
+++ tests/Amadeus/Client/Struct/HeaderV2/SessionTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * amadeus-ws-client
+ *
+ * Copyright 2015 Amadeus Benelux NV
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package Amadeus
+ * @license https://opensource.org/licenses/Apache-2.0 Apache 2.0
+ */
+
+namespace Test\Amadeus\Client\Struct\HeaderV2;
+
+use Amadeus\Client\Struct\HeaderV2\Session;
+use Test\Amadeus\BaseTestCase;
+
+/**
+ * SessionTest
+ *
+ * @package Test\Amadeus\Client\Struct\HeaderV2
+ * @author Dieter Devlieghere <dieter.devlieghere@benelux.amadeus.com>
+ */
+class SessionTest extends BaseTestCase
+{
+    public function testCanMakeSessionHeaderStructure()
+    {
+        $header = new Session('34J2KML4J', 3, 'FHMZROEZJHKJDHSKJLDQH');
+
+        $this->assertInstanceOf('\Amadeus\Client\Struct\HeaderV2\Session', $header);
+        $this->assertEquals('34J2KML4J', $header->SessionId);
+        $this->assertEquals(3, $header->SequenceNumber);
+        $this->assertEquals('FHMZROEZJHKJDHSKJLDQH', $header->SecurityToken);
+    }
+}
