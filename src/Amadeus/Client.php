c066c2cf176783b5f37d55708420cfbbc0424f69
Dieter Devlieghere <dieter@dermika.be>
2018-02-07 10:02:35 +0100

    Switching version marker on master to 1.7.0-dev

--- src/Amadeus/Client.php
+++ src/Amadeus/Client.php
@@ -57,7 +57,7 @@ class Client extends Base
      *
      * @var string
      */
-    const VERSION = "1.6.0";
+    const VERSION = "1.7.0-dev";
 
     /**
      * An identifier string for the library (to be used in Received From entries)
