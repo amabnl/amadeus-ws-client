e1dfe6a042295fe1db809abcee5c994deb10eac3
Patrick Kilter <patrick@proappers.com>
2018-03-27 09:07:35 +0200

    fixed search option call refs #168

--- src/Amadeus/Client/Struct/Fare/MasterPricerExpertSearch.php
+++ src/Amadeus/Client/Struct/Fare/MasterPricerExpertSearch.php
@@ -25,7 +25,7 @@ namespace Amadeus\Client\Struct\Fare;
 use Amadeus\Client\RequestOptions\Fare\MPFareFamily;
 use Amadeus\Client\RequestOptions\Fare\MPItinerary;
 use Amadeus\Client\RequestOptions\FareMasterPricerCalendarOptions;
-use Amadeus\Client\RequestOptions\FareMasterPricerTbSearch;
+use Amadeus\Client\RequestOptions\FareMasterPricerExSearch;
 use Amadeus\Client\RequestOptions\TicketAtcShopperMpTbSearchOptions;
 use Amadeus\Client\Struct\Fare\MasterPricer;
 
