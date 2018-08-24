691cfc588c80ae15a9c2b9d7afc11a017b59a092
arvind.p <arvind.pandey@qtechsoftware.com>
2018-08-24 12:37:47 +0530

    changes

--- src/Amadeus/Client/RequestOptions/ServiceIntegratedCatalogueOptions.php
+++ src/Amadeus/Client/RequestOptions/ServiceIntegratedCatalogueOptions.php
@@ -26,7 +26,7 @@ namespace Amadeus\Client\RequestOptions;
  * Service_IntegratedCatalogue Request Options
  *
  * @package Amadeus\Client\RequestOptions
- * @author Arvind Pandey <arvindpandey87@gmail.com>
+ * @author Dieter Devlieghere <dermikagh@gmail.com>
  */
 class ServiceIntegratedCatalogueOptions extends ServiceIntegratedPricingOptions
 {
